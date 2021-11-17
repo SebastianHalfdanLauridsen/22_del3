94f781e35357257ca26356a4758fbe07a0767609
Hamsterpants <kevinhoejgaard@gmail.com>
2021-11-17 13:27:11 +0100

    Added cardSetup(), playerSetup(), runGame(), playRound(), playTurn(), GUIStreetAction(), fieldAction(), movePlayer(), getRealNewIndex()

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -40,8 +40,176 @@ public class Game {
 
         this.deck = new Deck();
 
-        //playerSetup();
+        playerSetup();
         board.displayScoreboard(gui, playerManager);
+    }
+
+
+    private void cardSetup() {
+        /*
+        deck.addCard(Main.getLanguage().getString("cardName"),
+                Main.getLanguage().getString("cardDescription1"),
+                new MoveCard(23, board, this)
+        );
+        deck.addCard(Main.getLanguage().getString("cardName"),
+                Main.getLanguage().getString("cardDescription2"),
+                new PayBankCard(2, bank)
+        );
+        deck.addCard(Main.getLanguage().getString("cardName"),
+                Main.getLanguage().getString("cardDescription3"),
+                new GetBankMoneyCard(2,bank)
+        );
+        deck.addCard(Main.getLanguage().getString("cardName"),
+                Main.getLanguage().getString("cardDescription4"),
+                new GetAllPlayerMoneyCard(1, bank, playerManager)
+        );
+        deck.addCard("MoveMax",
+                "Hey",
+                new MoveMaxCard(5, gui, board, this)
+        );*/
+        /*
+        deck.addCard("SJANSE",
+                "FRI FELT! Flytt til et oransje felt. Hvis den er tilgjengelig, får du den GRATIS! Ellers må du BETALE husleie til eieren.",
+                new MoveGetFreeCard(10,11)
+        );
+        deck.addCard("SJANSE",
+                "FRI FELT! Flytt til et lyseblått felt. Hvis den er tilgjengelig, får du den GRATIS! Ellers må du BETALE husleie til eieren.",
+                new MoveGetFreeCard(4,5)
+        );
+        deck.addCard("SJANSE",
+                "FRI FELT! Flytt til et rødt felt. Hvis den er tilgjengelig, får du den GRATIS! Ellers må du BETALE husleie til eieren.",
+                new MoveGetFreeCard(13,14)
+        );
+
+        deck.addCard("SJANSE",
+                "Flytt opptil 5 felter.",
+                new MoveMaxCard(5)
+        );
+         */
+        deck.shuffleCards();
+    }
+
+    private void playerSetup() {
+        board.displayStartUI();
+        //create players
+        for(int i = 0; i < board.getUserCount(); i++) {
+            String nextPlayerName = board.getPlayerNames(i);
+            playerManager.createPlayer(nextPlayerName,35, cars.getCars()[i]);
+        }
+        //display cars
+        for(int i = 0; i < playerManager.getPlayerCount(); i++) {
+            GUI_Player playerObject = playerManager.getPlayers(i).getGUIPlayer();
+            GUI_Field startFieldObject = gui.getFields()[START_FIELD];
+            board.displayCar(playerObject, startFieldObject);
+        }
+    }
+
+    public void runGame(){
+        cardSetup();
+        sleep();
+        hand = new Hand(1, 6);
+
+        int currentFieldPos = 0;
+        GUI_Field currentField = gui.getFields()[currentFieldPos];
+        GUI_Field newField = currentField;
+
+        while( !(bank.playerHasWon()) ) {
+            playRound();
+        }
+        board.displayEndGame(bank.getWinningPlayer());
+    }
+
+    /**
+     * //TODO
+     */
+    private void playRound() {
+        for (int playerIndex = 0; playerIndex < playerManager.getPlayerCount(); playerIndex++) {
+            Player currentPlayer = playerManager.getPlayers(playerIndex);
+            playTurn(currentPlayer);
+        }
+    }
+
+    private void playTurn(Player currentPlayer) {
+        int currentFieldIndex = currentPlayer.getFieldPosition();
+
+        sleep();
+        gui.showMessage(currentPlayer.getGUIPlayer().getName() + Main.getLanguage().getString("rollMessage"));
+        hand.roll();
+
+        int dieFace = hand.getDice().get(0).getFace();
+        int diceSum = hand.sum();
+
+        //select field
+        int newFieldIndex = currentFieldIndex + diceSum;//getNewFieldIndex(currentFieldIndex, diceSum);
+
+        board.displayDie(gui, dieFace);
+
+        board.displayMovingPlayer(currentPlayer.getGUIPlayer(), diceSum, currentFieldIndex);
+        movePlayer(currentPlayer, newFieldIndex);
+        newFieldIndex = getRealNewIndex(currentFieldIndex, diceSum);
+
+        fieldAction(currentPlayer, newFieldIndex);
+    }
+
+    public void GUIStreetAction(Player player, int field) {
+        Player fieldOwner = bank.isOwned(field);
+        if (fieldOwner == null) {
+            bank.buyField(field, player);
+        } else if (fieldOwner != player) {
+            bank.payFieldRent(player, fieldOwner, (GUI_Street) fields.getFields()[field]);
+        }
+    }
+
+    private void fieldAction(Player player, int field) {
+        String fieldType = fields.getFields()[field].getClass().getSimpleName();
+        switch (fieldType) {
+            case "GUI_Street" -> {
+                System.out.println("GUI_Street!");
+                GUIStreetAction(player, field);
+            }
+            case "GUI_Chance" -> {
+                System.out.println("GUI_Chance!");
+
+                AbstractCard card = deck.drawCard();
+                String description = "";
+                try {
+                    description = card.getDescription();
+                } catch (NullPointerException e){
+                    Main.getLogr().log(Level.INFO, "Card that was drawn was null");
+                    e.getStackTrace();
+                    System.exit(-1);
+                }
+                System.out.println("Player " + player.getGUIPlayer().getName() + " drew chancecard" + "\n\""+ description + "\"");
+
+                gui.displayChanceCard(description);
+                Game.sleep();
+                card.action(player);
+            }
+            case "GUI_Jail" -> {
+                System.out.println("JÆEL");
+            }
+            case "GUI_Start" -> {
+                System.out.println("start field!");
+            }
+            default -> System.out.println("what");
+        }
+    }
+
+    public void movePlayer(Player player, int field) {
+        if (field >= MAX_FIELDS) {
+            bank.changeBalance(player,CROSS_START_MONEY);
+            player.setFieldPosition(field - MAX_FIELDS);
+        } else {
+            player.setFieldPosition(field);
+        }
+    }
+
+    private int getRealNewIndex(int currentFieldIndex, int diceSum) {
+        if ((currentFieldIndex + diceSum) >= MAX_FIELDS) {
+            return currentFieldIndex + diceSum - MAX_FIELDS;
+        } else {
+            return currentFieldIndex + diceSum;
+        }
     }
 
     public static void sleep(){
