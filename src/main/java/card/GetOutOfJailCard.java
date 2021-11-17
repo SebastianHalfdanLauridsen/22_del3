210016c4d8d8cb2d3fdb459381200ed29fc3c806
Jackie <Jackie_Trinh@hotmail.com>
2021-11-17 15:25:28 +0100

    Har tilføjet GetOutOfJailCard, men positionen af koden skal ændres. noter i koden.

--- src/main/java/card/GetOutOfJailCard.java
+++ src/main/java/card/GetOutOfJailCard.java
@@ -3,5 +3,26 @@ package card;
 //TODO implement card function
 // - https://drive.google.com/file/d/1ymv0T5xWIvprTZkSO6DtWz9byFvZA9h3/view
 
+/*
+
+//der skal være en getOutOfJailCount på hver spiller, som koden så skal kunne ændre værdien på.
+
+
+//tilføjer et count til getOutOfJailCounter (hvor jail field'et så skal tjekke om brugeren har et count eller ej)
 public class GetOutOfJailCard extends AbstractCard{
+    getOutOfJailCount = 1;
 }
+
+//ser om spilleren har et get out of jail card. (mangler funktionen at skip en tur og brug af kortet)
+//Den her skal være i sammenhæng med jail field, da den tjekker om spilleren har kortet eller ej.
+
+
+if(getOutOfJailCount<1){
+    System.out.println(you have no get out of jail card, your turn is skipped);
+    }
+
+else{
+    System.out.println(you have a get out of jail card, you got out of jail);
+    getOutOfJailCount = 0; //fjerner kortet fra spilleren (da den blev brugt)
+    }
+ */
