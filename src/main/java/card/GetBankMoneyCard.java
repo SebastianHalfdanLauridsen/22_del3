cd52e3f621d9c1bc4afdd51e5331bdf32bebebaf
Deniz Isikli <s215818@dtu.dk>
2021-11-18 16:15:39 +0100

    Added GetBankMoneyCard, action()

--- src/main/java/card/GetBankMoneyCard.java
+++ src/main/java/card/GetBankMoneyCard.java
@@ -1,7 +1,23 @@
 package card;
+import player.Bank;
+import player.Player;
 
 //TODO implement card function
 // - https://drive.google.com/file/d/1ymv0T5xWIvprTZkSO6DtWz9byFvZA9h3/view
 
+
+
 public class GetBankMoneyCard extends AbstractCard{
+    int paymentAmount;
+    private Bank bank;
+
+
+    public GetBankMoneyCard(int paymentAmount, Bank bank) {
+        this.paymentAmount = paymentAmount;
+        this.bank = bank;
+    }
+
+    public void action(Player player){
+        bank.changeBalance(player, paymentAmount);
+    }
 }
