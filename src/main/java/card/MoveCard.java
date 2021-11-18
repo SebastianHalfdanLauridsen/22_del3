745373beca2d5033c8d79eec3941965bba021a84
Deniz Isikli <s215818@dtu.dk>
2021-11-18 16:14:54 +0100

    Added MoveCard, action()

--- src/main/java/card/MoveCard.java
+++ src/main/java/card/MoveCard.java
@@ -1,8 +1,22 @@
 package card;
+import player.Player;
 
 //TODO implement card function
 // - https://drive.google.com/file/d/1ymv0T5xWIvprTZkSO6DtWz9byFvZA9h3/view
 // - https://drive.google.com/file/d/15oSUaFK5NtryM21fVlUwhGYGYiCACye7/view
 
+
+
 public class MoveCard extends AbstractCard{
+    int Strandpromenaden;
+
+    public MoveCard(int Strandpromenaden) {
+        this.Strandpromenaden = Strandpromenaden;
+
+    }
+
+    public void action(Player player){
+        player.setFieldPosition(Strandpromenaden);
+    }
+
 }
