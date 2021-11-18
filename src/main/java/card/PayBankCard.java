452b13a695d26af30c653b3e8cbedd671ee75427
Deniz Isikli <s215818@dtu.dk>
2021-11-18 16:15:12 +0100

    Added PayBankCard, action()

--- src/main/java/card/PayBankCard.java
+++ src/main/java/card/PayBankCard.java
@@ -1,7 +1,24 @@
 package card;
+import player.Bank;
+import player.Player;
+
 
 //TODO implement card function
 // - https://drive.google.com/file/d/1ymv0T5xWIvprTZkSO6DtWz9byFvZA9h3/view
 
+
+
 public class PayBankCard extends AbstractCard{
+    int payAmount;
+    private Bank bank;
+
+    public PayBankCard(int payAmount, Bank bank) {
+        this.payAmount = payAmount;
+        this.bank = bank;
+
+    }
+
+    public void action(Player player) {
+        bank.changeBalance(player, payAmount);
+    }
 }
