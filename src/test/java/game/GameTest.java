fb54e8e90f216010ab713b4b110d985da2131d8b
Hamsterpants <kevinhoejgaard@gmail.com>
2021-11-19 02:23:23 +0100

    Created GameTest class

---
+++ src/test/java/game/GameTest.java
@@ -0,0 +1,14 @@
+package game;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class GameTest {
+
+    @Test
+    public void firstTest() {
+        Assert.assertTrue(true);
+    }
+
+}
