5ba42094b0900a8dc0310a404e8ec0b90c4fda88
SebastianHalfdanLauridsen <s215769@dtu.dk>
2021-11-21 00:49:44 +0100

    Added setup()

--- src/test/java/game/GameTest.java
+++ src/test/java/game/GameTest.java
@@ -5,6 +5,25 @@ import org.junit.Test;
 
 
 public class GameTest {
+    Game game;
+    Deck deck;
+    Board board;
+    PlayerManager playerManager;
+    Bank bank;
+    Cars cars;
+    GUI gui;
+
+    private void setup(){
+        Main.setLanguage();
+
+        game = new Game();
+        deck = game.getDeck();
+        board = game.getBoard();
+        playerManager = game.getPlayerManager();
+        bank = game.getBank();
+        cars = game.getCars();
+        gui = game.getGui();
+    }
 
     @Test
     public void firstTest() {
