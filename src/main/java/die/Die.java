ffbeb350e79cb0786b31402d0ea192bbfd618c33
SebastianHalfdanLauridsen <s215769@dtu.dk>
2021-11-20 20:03:26 +0100

    Refactored

--- src/main/java/die/Die.java
+++ src/main/java/die/Die.java
@@ -4,15 +4,17 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.concurrent.ThreadLocalRandom;
 
-//TODO determine which methods should be protected instead of public (maybe all of them?)
+/**
+ * //TODO
+ */
 public class Die {
     private static final Logger logr = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
     private int face;
     private int sides = 0;
 
-    //TODO maybe protected?
-    public Die(int face, int sides) {
+
+    protected Die(int face, int sides) {
         if(face < 1 || face > sides) {
             logr.log(Level.INFO, "The die's face value cannot be less than 1 " +
                     "or more than the amount of sides.");
@@ -21,8 +23,8 @@ public class Die {
         this.face = face;
         this.sides = sides;
     }
-    //TODO maybe protected?
-    public Die(int sides) {
+
+    protected Die(int sides) {
         if(sides < 1) {
             logr.log(Level.INFO, "The die cannot have less than 1 side.");
             return;
@@ -30,33 +32,18 @@ public class Die {
         this.sides = sides;
         roll();
     }
-    //TODO maybe protected?
+
     public Die() {
         this.sides = 6; //TODO GLOBAL VARIABLE WITH SIDES
         roll();
     }
-    //TODO maybe protected?
-    public void roll() {
+
+    protected void roll() {
         face = ThreadLocalRandom.current().nextInt(1,sides + 1);
     }
-    //TODO maybe protected?
-    //getters
+
     public int getFace() {
         return face;
     }
-    //TODO maybe protected?
-    //setters
-    public void setFace(int face) {
-        if(face < 1 || face > sides) {
-            logr.log(Level.INFO, "The die's face value cannot be less than 1 " +
-                    "or more than the amount of sides.");
-            return;
-        }
-        this.face = face;
-    }
-    //TODO maybe protected?
-    @Override
-    public String toString() {
-        return Integer.toString(face);
-    }
+
 }
