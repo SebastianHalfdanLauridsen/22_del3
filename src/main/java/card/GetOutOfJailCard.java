441c88c1f51e94d2361a8982763ca9ad44473a9a
SebastianHalfdanLauridsen <s215769@dtu.dk>
2021-11-21 00:45:55 +0100

    Added action()

--- src/main/java/card/GetOutOfJailCard.java
+++ src/main/java/card/GetOutOfJailCard.java
@@ -5,19 +5,9 @@ import player.Player;
  * //TODO
  */
 public class GetOutOfJailCard extends AbstractCard{
-    getOutOfJailCount = 1;
+
+    public void action(Player player) {
+        player.setJailCard(true);
+    }
 }
 
-//ser om spilleren har et get out of jail card. (mangler funktionen at skip en tur og brug af kortet)
-//Den her skal være i sammenhæng med jail field, da den tjekker om spilleren har kortet eller ej.
-
-
-if(getOutOfJailCount<1){
-    System.out.println(you have no get out of jail card, your turn is skipped);
-    }
-
-else{
-    System.out.println(you have a get out of jail card, you got out of jail);
-    getOutOfJailCount = 0; //fjerner kortet fra spilleren (da den blev brugt)
-    }
- */
