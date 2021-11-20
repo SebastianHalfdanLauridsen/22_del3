cea20c5c043105c97a0ca24671f06b7a74624291
SebastianHalfdanLauridsen <s215769@dtu.dk>
2021-11-21 00:47:05 +0100

    Added cardsTest()

--- src/test/java/card/CardsTest.java
+++ src/test/java/card/CardsTest.java
@@ -1,4 +1,48 @@
 package card;
 
+import org.junit.Test;
+import org.junit.Assert;
+import game.Board;
+import game.Game;
+import game.Main;
+import gui_main.GUI;
+import player.Bank;
+import player.Cars;
+import player.PlayerManager;
+
 public class CardsTest {
+    Game game;
+    Deck deck;
+    Board board;
+    PlayerManager playerManager;
+    Bank bank;
+    Cars cars;
+    GUI gui;
+
+    @Test
+    public void cardsTest() {
+        setup();
+
+        new Cards(deck, board, bank, playerManager, gui, game);
+
+        for (AbstractCard d : deck.getCards()) {
+            Game.sleep();
+            gui.displayChanceCard(d.getDescription());
+        }
+
+        Game.sleep(5000);
+    }
+
+    private void setup(){
+        Main.setLanguage();
+
+        game = new Game();
+
+        deck = game.getDeck();
+        board = game.getBoard();
+        playerManager = game.getPlayerManager();
+        bank = game.getBank();
+        cars = game.getCars();
+        gui = game.getGui();
+    }
 }
