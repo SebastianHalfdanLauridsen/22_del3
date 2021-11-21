86c47883f17303636f6351bc8121b6b220996faa
Hamsterpants <kevinhoejgaard@gmail.com>
2021-11-22 00:49:30 +0100

    Reworked Cards to be a record class, added language

--- src/main/java/card/Cards.java
+++ src/main/java/card/Cards.java
@@ -10,14 +10,8 @@ import player.PlayerManager;
 /**
  * //TODO
  */
-public class Cards {
-    private final Deck deck;
-    private final Board board;
-    private final Bank bank;
-    private final PlayerManager playerManager;
-    private final GUI gui;
-    private final Game game;
-
+public record Cards(Deck deck, Board board, Bank bank, PlayerManager playerManager,
+                    GUI gui, Game game) {
     public Cards(Deck deck, Board board, Bank bank, PlayerManager playerManager, GUI gui, Game game) {
         this.deck = deck;
         this.board = board;
@@ -43,61 +37,61 @@ public class Cards {
         );
         deck.addCard(Main.getLanguage().getString("cardName"),
                 Main.getLanguage().getString("cardDescription3"),
-                new GetBankMoneyCard(2,bank)
+                new GetBankMoneyCard(2, bank)
         );
 
         deck.addCard(Main.getLanguage().getString("cardName"),
                 Main.getLanguage().getString("cardDescription4"),
                 new GetPlayersMoneyCard(1, bank, playerManager)
         );
-        //TODO resource bundle language
-        deck.addCard("SJANSE",
-                "FRI FELT! Flytt til et oransje felt. Hvis den er tilgjengelig, får du den GRATIS! Ellers må du BETALE husleie til eieren.",
-                new MoveFieldGetFreeCard(10,11, gui, game, board, bank) //orange
+
+        deck.addCard(Main.getLanguage().getString("cardName"),
+                Main.getLanguage().getString("cardDescription5"),
+                new MoveFieldGetFreeCard(10, 11, gui, game, board, bank) //orange
         );
-        deck.addCard("SJANSE",
-                "FRI FELT! Flytt til et lyseblått felt. Hvis den er tilgjengelig, får du den GRATIS! Ellers må du BETALE husleie til eieren.",
-                new MoveFieldGetFreeCard(4,5, gui, game, board, bank) //light blue
+        deck.addCard(Main.getLanguage().getString("cardName"),
+                Main.getLanguage().getString("cardDescription6"),
+                new MoveFieldGetFreeCard(4, 5, gui, game, board, bank) //light blue
         );
-        deck.addCard("SJANSE",
-                "FRI FELT! Flytt til et rødt felt. Hvis den er tilgjengelig, får du den GRATIS! Ellers må du BETALE husleie til eieren.",
-                new MoveFieldGetFreeCard(13,14, gui, game, board, bank) //red
+        deck.addCard(Main.getLanguage().getString("cardName"),
+                Main.getLanguage().getString("cardDescription7"),
+                new MoveFieldGetFreeCard(13, 14, gui, game, board, bank) //red
         );
 
-        deck.addCard("SJANSE",
-                "FRI FELT! Flytt til et oransje eller grønt felt. Hvis den er tilgjengelig, får du den GRATIS! Ellers må du BETALE husleie til eieren.",
-                new MoveFieldGetFreeCard(10,11, 19, 20, gui, game, board, bank) //orange and green
+        deck.addCard(Main.getLanguage().getString("cardName"),
+                Main.getLanguage().getString("cardDescription8"),
+                new MoveFieldGetFreeCard(10, 11, 19, 20, gui, game, board, bank) //orange and green
         );
-        deck.addCard("SJANSE",
-                "FRI FELT! Flytt til et lyserødt eller blåt felt. Hvis den er tilgjengelig, får du den GRATIS! Ellers må du BETALE husleie til eieren.",
-                new MoveFieldGetFreeCard(7,8, 22, 23, gui, game, board, bank) //pink and darkblue
+        deck.addCard(Main.getLanguage().getString("cardName"),
+                Main.getLanguage().getString("cardDescription9"),
+                new MoveFieldGetFreeCard(7, 8, 22, 23, gui, game, board, bank) //pink and darkblue
         );
-        deck.addCard("SJANSE",
-                "FRI FELT! Flytt til et lyseblått eller rødt felt. Hvis den er tilgjengelig, får du den GRATIS! Ellers må du BETALE husleie til eieren.",
-                new MoveFieldGetFreeCard(4,5, 12, 13, gui, game, board, bank) //light blue and red
+        deck.addCard(Main.getLanguage().getString("cardName"),
+                Main.getLanguage().getString("cardDescription10"),
+                new MoveFieldGetFreeCard(4, 5, 13, 14, gui, game, board, bank) //light blue and red
         );
-        deck.addCard("SJANSE",
-                "FRI FELT! Flytt til et brunt eller gult felt. Hvis den er tilgjengelig, får du den GRATIS! Ellers må du BETALE husleie til eieren.",
-                new MoveFieldGetFreeCard(1,2, 16, 17, gui, game, board, bank) //brown and yellow
-        );
-
-        deck.addCard("SJANSE",
-                "Flytt opptil 5 felter.",
-                new MoveMaxFiveCard(gui,board, game)
+        deck.addCard(Main.getLanguage().getString("cardName"),
+                Main.getLanguage().getString("cardDescription11"),
+                new MoveFieldGetFreeCard(1, 2, 16, 17, gui, game, board, bank) //brown and yellow
         );
 
-        deck.addCard("SJANSE",
-                "Move or draw",
+        deck.addCard(Main.getLanguage().getString("cardName"),
+                Main.getLanguage().getString("cardDescription12"),
+                new MoveMaxFiveCard(gui, board, game)
+        );
+
+        deck.addCard(Main.getLanguage().getString("cardName"),
+                Main.getLanguage().getString("cardDescription13"),
                 new MoveOrDrawCard(1, gui, board, game)
         );
 
-        deck.addCard("SJANSE",
-                "Move to start get M2",
+        deck.addCard(Main.getLanguage().getString("cardName"),
+                Main.getLanguage().getString("cardDescription14"),
                 new MoveCard(0, board, game) //start
         );
 
-        deck.addCard("SJANSE",
-                "Du løslades uden omkostninger. Behold dette kort, indtil du får brug for det.",
+        deck.addCard(Main.getLanguage().getString("cardName"),
+                Main.getLanguage().getString("cardDescription15"),
                 new GetOutOfJailCard());
     }
 }
