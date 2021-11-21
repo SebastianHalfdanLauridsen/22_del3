013a1bf30335d9b3ab5fbdb35fb556d710fbedb2
SebastianHalfdanLauridsen <s215769@dtu.dk>
2021-11-21 23:09:08 +0100

    Minor change

--- src/main/java/card/MoveCard.java
+++ src/main/java/card/MoveCard.java
@@ -12,13 +12,11 @@ public class MoveCard extends AbstractCard{
     private final Board board;
     private final Game game;
 
-
     public MoveCard(int fieldIndex, Board board, Game game) {
         this.fieldIndex = fieldIndex;
         this.board = board;
         this.game = game;
     }
-
 
     public void action(Player player) {
         Game.sleep();
